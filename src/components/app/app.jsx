67ac84dafe924aa60cfa413eb444e9e94364cac9
Vladimir <vokar90@yandex.ru>
2024-05-20 17:49:42 +0300

    feat: add header

---
+++ src/components/app/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import styles from './app.module.css';
+import AppHeader from '../header/app-header';
+
+function App() {
+  return (
+    <>
+      <AppHeader/>
+
+    </>
+  );
+}
+
+export default App;
